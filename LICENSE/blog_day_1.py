a0e5dc617e5239a9cf4ae521818809b0999053b5
SuperMax999 <1649514591@qq.com>
2022-05-25 15:48:36 +0800

    initialization

---
+++ LICENSE/blog_day_1.py
@@ -0,0 +1,31 @@
+# 博客APP编写 day-1: 搭建开发环境
+
+
+# 本项目将尝试编写一个web app来实现个人博客
+# 文件具体存放可参考 https://github.com/yzyly1992/2019_Python_Web_Dev
+
+# 1.项目所需的模板
+# (1) aiohttp异步框架 (2) jinja2前端模板引擎 (3)aiomysql异步数据库 (4)轻量级标记语言markdown, 将文本转换为有效的HTML
+
+# 2.项目结构
+# Python_project_blog/  <-- 根目录
+# |
+# +- backup/               <-- 备份目录
+# |
+# +- conf/                 <-- 配置文件
+# |
+# +- dist/                 <-- 打包目录
+# |
+# +- www/                  <-- Web目录，(存放.py文件)
+# |  |
+# |  +- static/            <-- 存放静态文件
+# |  |
+# |  +- templates/         <-- 存放模板文件
+# |
+# +- ios/                  <-- 存放iOS App工程
+# |
+# +- LICENSE               <-- 代码LICENSE
+
+
+# 3. github
+# 参考廖雪峰git教程，建立git仓库并同步到GitHub, 保证代码储存及修改的安全。
